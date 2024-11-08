50aa93f750115d9e7b4984de90341284b44b35ab
thehilmisu <ozcanhakanzi@mgmail.com>
2024-11-09 00:44:33 +0100

    Graphics and some physics components added

--- src/Core/Engine.cpp
+++ src/Core/Engine.cpp
@@ -1,5 +1,7 @@
 #include "Engine.h"
 #include "Log.h"
+#include "../Graphics/TextureManager.h"
+#include "../Physics/Transform.h"
 
 
 Engine* Engine::s_Instance = nullptr;
@@ -16,30 +18,71 @@ bool Engine::Init()
 
     CORE_INFO("SDL Initialized successfully");
 
+    m_Window = SDL_CreateWindow("Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if(m_Window == nullptr)
+    {
+        CORE_ERROR("Failed to Create Window: {0}", SDL_GetError());
+        return false;
+    }
+
+    m_Renderer = SDL_CreateRenderer(m_Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if(m_Renderer == nullptr)
+    {
+        CORE_ERROR("Failed to Create Renderer: {0}", SDL_GetError());
+        return false;
+    }
+
+    TextureManager::GetInstance()->Load("tree", "/home/thehilmisu/Desktop/Workdir/2DGameEngine/assets/tree.png");
+
+
     return m_IsRunning = true;
 }
 
 bool Engine::Clean()
 {
+    TextureManager::GetInstance()->Clean();
+    SDL_DestroyRenderer(m_Renderer);
+    SDL_DestroyWindow(m_Window);
+    IMG_Quit();
+    SDL_Quit();
+
     return true;
 }
 
 void Engine::Quit()
 {
-
-}
+    m_IsRunning = false;
+}   
 
 void Engine::Update()
 {
-    CORE_INFO("Updating...");
+    //CORE_INFO("Updating...");
 }
 
 void Engine::Render()
 {
+    SDL_SetRenderDrawColor(m_Renderer, 124, 218, 254, 255);
+    SDL_RenderClear(m_Renderer);
 
+    TextureManager::GetInstance()->Draw("tree", 10, 10, 418, 1024);
+
+    SDL_RenderPresent(m_Renderer);
 }
 
 void Engine::Events()
 {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    
+    switch (event.type)
+    {
+        case SDL_QUIT:
+            Quit();
+            break;
+        
+        default:
+            break;
+    }
+
 
 }
---
+++ src/Graphics/TextureManager.cpp
@@ -0,0 +1,56 @@
+#include "TextureManager.h"
+#include "../Core/Engine.h"
+#include "../Core/Log.h"
+
+TextureManager* TextureManager::s_Instance = nullptr;
+
+bool TextureManager::Load(std::string id, std::string  filename)
+{
+    SDL_Surface* surface = IMG_Load(filename.c_str());
+    if(surface == nullptr)
+    {
+        CORE_ERROR("failed to load texture : {0}, {1}", filename.c_str(), SDL_GetError());
+        return false;
+    }
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(Engine::GetInstance()->GetRenderer(), surface);
+    if(texture == nullptr)
+    {
+        CORE_ERROR("failed to create texture from surface : {0}", SDL_GetError());
+        return false;
+    }
+
+    m_TextureMap[id] = texture;
+    CORE_INFO("texture loaded with id : {0}", id);
+
+    return true;
+}
+
+void TextureManager::Draw(std::string id, int x, int y, int width, int height, 
+                        SDL_RendererFlip flip)
+{
+    SDL_Rect srcRect = {0, 0, width, height};
+    SDL_Rect dstRect = {x, y, width, height};
+
+    SDL_RenderCopyEx(Engine::GetInstance()->GetRenderer(), m_TextureMap[id], &srcRect, &dstRect, 0, nullptr, flip);
+
+}
+
+void TextureManager::Drop(std::string id)
+{
+    SDL_DestroyTexture(m_TextureMap[id]);
+    m_TextureMap.erase(id);
+    CORE_WARN("texture removed, id : {0}", id);
+}
+
+void TextureManager::Clean()
+{
+    std::map<std::string, SDL_Texture*>::iterator it;
+    for(it = m_TextureMap.begin(); it != m_TextureMap.end(); it++)
+        SDL_DestroyTexture(it->second);
+
+    m_TextureMap.clear();
+
+    CORE_INFO("texture map cleaned...");
+}
+
